a2d8d88722348ffd73df377afac42a744308c8d8
HelloZJun <31958275+HelloZJun@users.noreply.github.com>
2019-03-01 10:56:41 +0800

    Update websocket.php

--- websocket.php
+++ websocket.php
@@ -1,6 +1,5 @@
 <?php
 class WebsocketTest {
-    public $server;
     public function __construct() {
         static $user_list=[];//使用全局静态变量存储用户名
         $this->server = new Swoole\WebSocket\Server("192.168.61.130", 9000);
@@ -16,15 +15,17 @@ class WebsocketTest {
                 $arr['user_list']=$user_list;
                 $arr['num']=count($arr['user_list']);
                 $data=json_encode($arr);
+                foreach ($this->server->connections as $fd) {
+                    $this->server->push($fd, "$data");
+                }
             }
-            else if($arr['type']=='user'){
+            if($arr['type']=='user'){
                 $arr['from']=$user_list["{$frame->fd}"];
                 $data=json_encode($arr);
+                foreach ($this->server->connections as $fd) {
+                    $this->server->push($fd, "$data");
+                }
             }
-            else{
-                return;
-            }
-            pushmsg($data);
         });
         $this->server->on('close', function ($ser, $fd) {
             $info=$this->server->connection_info($fd);
@@ -38,22 +39,20 @@ class WebsocketTest {
                 $arr['user_list']=$user_list;
                 $arr['num']=count($arr['user_list']);
                 $data=json_encode($arr);
-                pushmsg($data);
+                foreach ($this->server->connections as $fd){
+                    $this->server->push($fd, "$data");
+                }
             }
         });
         $this->server->on('request', function ($request, $response) {
             // 接收http请求从get获取message参数的值，给用户推送
             // $this->server->connections 遍历所有websocket连接用户的fd，给所有用户推送
             $data=json_encode($request->post);
-            pushmsg($data);
+            foreach ($this->server->connections as $fd) {
+                $this->server->push($fd, $data);
+            }
         });
         $this->server->start();
-    }
-
-    function pushmsg($data){
-        foreach ($this->server->connections as $fd) {
-            $this->server->push($fd, $data);
-        }
     }
 }
 new WebsocketTest();
