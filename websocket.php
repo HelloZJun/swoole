f45bf0998a990589f19e35ad20b7b44078f4f193
HelloZJun <31958275+HelloZJun@users.noreply.github.com>
2019-03-01 10:07:03 +0800

    Update websocket.php

--- websocket.php
+++ websocket.php
@@ -30,17 +30,18 @@
         });
         $this->server->on('close', function ($ser, $fd) {
             $info=$ser->connection_info($fd);
-            echo $info['WebSocket_status'];
-            echo "client {$fd} closed\n";
-            global $user_list;
-            $arr['content']=$user_list["{$fd}"];
-            unset($user_list["{$fd}"]);
-            $arr['type']='close';
-            $arr['user_list']=$user_list;
-            $arr['num']=count($arr['user_list']);
-            $data=json_encode($arr);
-            foreach ($this->server->connections as $fd){
-                $this->server->push($fd, "$data");
+            if($info['WebSocket_status']){
+                echo "client {$fd} closed\n";
+                global $user_list;
+                $arr['content']=$user_list["{$fd}"];
+                unset($user_list["{$fd}"]);
+                $arr['type']='close';
+                $arr['user_list']=$user_list;
+                $arr['num']=count($arr['user_list']);
+                $data=json_encode($arr);
+                foreach ($this->server->connections as $fd){
+                    $this->server->push($fd, "$data");
+                }
             }
         });
         $this->server->on('request', function ($request, $response) {
